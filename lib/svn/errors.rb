9eda857033c11817cd84e759cd68d47725106bbc
Ryan Blue <rdblue@gmail.com>
2011-10-03 18:14:19 -0600

    Adding more to the basic error code.
    Now the "best" error message will be used and errors are cleared to avoid
    memory leaks using FFI::ManagedStruct.

--- lib/svn/errors.rb
+++ lib/svn/errors.rb
@@ -14,13 +14,13 @@ module Svn
     attr_reader :c_error
 
     def initialize( c_error )
-      @message = c_error[:message]
+      super( c_error.best_message )
       @c_error = c_error
     end
 
   end
 
-  class CError < FFI::Struct
+  class CError < FFI::ManagedStruct
     layout(
         :apr_error, :int,
         :message, :string,
@@ -30,12 +30,41 @@ module Svn
         :line, :long
       )
 
-    module C
-      extend FFI::Library
-      # error functions and ffi_lib entry
+    class << self
+      def release( ptr )
+        C.clear( ptr )
+      end
     end
 
-    # TODO: add a release method that calls svn_error_clear
+    # returns the most accurate message for an error
+    def best_message
+      # create a buffer, which may be used to hold the best message
+      buf = FFI::MemoryPointer.new( 1024 )
+      msg = C.best_message( self, buf, 1024 )
+
+      # return a duplicate of msg, since it may be stored in the buffer
+      # allocated above
+      msg.dup
+    end
+
+    module C
+      extend FFI::Library
+      ffi_lib 'libsvn_subr-1.so.1'
+      # error functions and ffi_lib entry
+
+      typedef CError.by_ref, :error
+      typedef :int, :size
+
+      attach_function :best_message,
+          :svn_err_best_message,
+          [ :error, :buffer_inout, :size ],
+          :string
+
+      attach_function :clear,
+          :svn_error_clear,
+          [ :error ],
+          :void
+    end
 
   end
 
