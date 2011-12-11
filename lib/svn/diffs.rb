840829189c0857e4e03c386fd82a58fb696c79d5
Ryan Blue <rdblue@gmail.com>
2011-12-11 17:56:36 +0000

    Adding support for memory string diffs.
    Added Diff.string_diff(...) to compare two in-memory CountedStrings to support
    SVN file content diffs. Updated Diff#unified to output the result of
    string_diff diffs, which uses a different SVN library call,
    mem_string_diff_unified.
    
    Made minor changes to the existing diff library:
    * Updated methods that return a StringIO object to rewind it first
    * Moved FileOptionsStruct#from_hash to the FileOptions proxy object
    * Cleaned up the original Diff.file_diff implementation

--- lib/svn/diffs.rb
+++ lib/svn/diffs.rb
@@ -7,16 +7,44 @@ module Svn #:nodoc:
   class Diff < FFI::AutoPointer
 
     class << self
-      def diff( original_path, modified_path, options={}, pool=RootPool )
+      def string_diff( original, modified, options={}, pool=RootPool )
         options = FileOptions.from_hash( options ) if options.is_a? Hash
+        original = CountedString.from_string( original )
+        modified = CountedString.from_string( modified )
 
-        out = FFI::MemoryPointer.new( :pointer )
+        out = FFI::MemoryPointer.new( Diff )
 
-        err = C.file_diff( out, original_path, modified_path, options, pool )
-
-        Error.check_and_raise( err )
+        Error.check_and_raise(
+            C.string_diff( out, original, modified, options, pool )
+          )
 
         d = new( out.read_pointer )
+
+        return nil if d.null?
+
+        d.type = :string
+        d.original = original
+        d.modified = modified
+        d.options = options
+        d.pool = pool
+
+        return d
+      end
+
+      def file_diff( original_path, modified_path, options={}, pool=RootPool )
+        options = FileOptions.from_hash( options ) if options.is_a? Hash
+
+        out = FFI::MemoryPointer.new( Diff )
+
+        Error.check_and_raise(
+            C.file_diff( out, original_path, modified_path, options, pool )
+          )
+
+        d = new( out.read_pointer )
+
+        return nil if d.null?
+
+        d.type = :file
         d.original_path = original_path
         d.modified_path = modified_path
         d.options = options
@@ -31,7 +59,10 @@ module Svn #:nodoc:
       end
     end
 
+    attr_accessor :type
+    attr_accessor :original
     attr_accessor :original_path
+    attr_accessor :modified
     attr_accessor :modified_path
     attr_accessor :options
     attr_accessor :pool
@@ -44,17 +75,62 @@ module Svn #:nodoc:
       ( C.has_conflicts( self ) == 1 )
     end
 
-    def unified(
-        out_stream=nil, original_header=nil, modified_header=nil,
-        path_strip=nil, encoding='utf-8'
-      )
-      out_stream ||= StringIO.new
+    def unified( *args )
+      # keep these in scope
+      out_stream = nil
+      options = nil
+      pool = nil
 
-      Error.check_and_raise( C.output_unified(
-          Svn::Stream.wrap_io( out_stream ), self, original_path, modified_path,
-          original_header, modified_header, encoding, path_strip,
-          options[:show_c_function], pool
-        ) )
+      case args.size
+      when 0
+        # use all defaults
+      when 1
+        if args.first.is_a? Hash
+          options = args.first
+        elsif args.first.is_a? IO or args.first.is_a? StringIO
+          out_stream = args.first
+        elsif args.first.is_a? Pool
+          pool = args.first
+        end
+      when 2, 3
+        out_stream, options, pool = args
+      else
+        raise ArgumentError, "wrong number of arguments (#{args.size} for 3)"
+      end
+
+      # defaults
+      out_stream ||= StringIO.new
+      options ||= {}
+      pool ||= RootPool
+
+      # get common options
+      encoding = options[:encoding] || 'utf-8'
+      original_header = options[:original_header]
+      modified_header = options[:modified_header]
+
+      case type
+      when :string
+        with_diff_header = ( options[:with_diff_header] ? 1 : 0 )
+
+        Error.check_and_raise( C.string_output_unified(
+            Svn::Stream.wrap_io( out_stream ), self,
+            original_header, modified_header, encoding,
+            original, modified, pool
+          ) )
+
+      when :file
+        path_strip = options[:path_strip]
+        show_c_function = ( options[:show_c_function] ? 1 : 0 )
+
+        Error.check_and_raise( C.file_output_unified(
+            Svn::Stream.wrap_io( out_stream ), self,
+            original_path, modified_path,
+            original_header, modified_header, encoding, path_strip,
+            show_c_function, pool
+          ) )
+      end
+
+      out_stream.rewind if out_stream.is_a? StringIO
 
       out_stream
     end
@@ -65,29 +141,31 @@ module Svn #:nodoc:
           :ignore_eol_style, :int,
           :show_c_function, :int
         )
-
-      def self.from_hash( hash )
-        options = new
-        hash.each_pair do |key, val|
-          # add a check if key is in members?
-          options[key] = val
-        end
-        options
-      end
     end
 
     # create a mapped type for use elsewhere
     FileOptions = FileOptionsStruct.by_ref
+
+    def FileOptions.from_hash( hash )
+      options = FileOptionsStruct.new
+      hash.each_pair do |key, val|
+        # add a check if key is in members?
+        options[key] = val
+      end
+      options
+    end
 
     module C
 
       extend FFI::Library
       ffi_lib 'libsvn_diff-1.so.1'
 
+      typedef :pointer, :out_pointer
       typedef Pool, :pool
       typedef CError.by_ref, :error
       typedef Stream, :stream
       typedef FileOptions, :file_options
+      typedef CountedString, :counted_string
       typedef Diff, :diff
 
       enum :whitespace, [
@@ -96,24 +174,34 @@ module Svn #:nodoc:
           :all      # ignore all whitespace chars
         ]
 
+      # diff functions
       attach_function :file_diff,
           :svn_diff_file_diff_2,
-          [:pointer, :string, :string, :file_options, :pool],
+          [ :out_pointer, :string, :string, :file_options, :pool ],
           :error
-      attach_function :file_diff3,
-          :svn_diff_file_diff3_2,
-          [:pointer, :string, :string, :string, :file_options, :pool],
+
+      attach_function :string_diff,
+          :svn_diff_mem_string_diff,
+          [ :out_pointer, :counted_string, :counted_string, :file_options,
+            :pool ],
           :error
-      attach_function :file_diff4,
-          :svn_diff_file_diff4_2,
-          [:pointer, :string, :string, :string, :string, :file_options, :pool],
-          :error
-      attach_function :output_unified,
-          :svn_diff_file_output_unified3,
-          [:stream, :diff, :string, :string, :string, :string, :string, :string, :int, :pool],
-          :error
+
+      # diff inspection
       attach_function :is_changed, :svn_diff_contains_diffs, [:diff], :int
       attach_function :has_conflicts, :svn_diff_contains_conflicts, [:diff], :int
+
+      # output functions
+      attach_function :file_output_unified,
+          :svn_diff_file_output_unified3,
+          [ :stream, :diff, :string, :string, :string, :string, :string,
+            :string, :int, :pool ],
+          :error
+
+      attach_function :string_output_unified,
+          :svn_diff_mem_string_output_unified,
+          [ :stream, :diff, :string, :string, :string,
+            :counted_string, :counted_string, :pool ],
+          :error
     end
 
   end
